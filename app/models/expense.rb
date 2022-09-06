9e294e8d49b42560d3d525bb1f810103cb5ef000
Juan Rosario <jrosario19@hotmail.com>
2022-09-06 15:29:59 -0400

    Add category and expense model

---
+++ app/models/expense.rb
@@ -0,0 +1,9 @@
+class Expense < ApplicationRecord
+    belongs_to :user
+    has_many :category_expenses
+    has_many :categories, through: :category_expenses
+  
+    validates :name, presence: true
+    validates :amount, presence: true, numericality: { greater_than: 0 }
+  end
+  
