5a7d0d86859b2f1acbe812f7cbf1d2f2f65383be
Juan Rosario <jrosario19@hotmail.com>
2022-09-06 16:03:31 -0400

    Modify data type for amount in expense

--- db/migrate/20220906162623_create_expenses.rb
+++ db/migrate/20220906162623_create_expenses.rb
@@ -2,7 +2,7 @@ class CreateExpenses < ActiveRecord::Migration[7.0]
   def change
     create_table :expenses do |t|
       t.string :name
-      t.string :amount
+      t.float :amount
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_164334) do
 
   create_table "expenses", force: :cascade do |t|
     t.string "name"
-    t.string "amount"
+    t.float "amount"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
